bbc963b8ee650cc2a0cb122e94c1bceab4f70ade
AhmedNazem <aahhaammdd55@gmail.com>
2024-12-21 01:28:42 +0300

    create message model and it's endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 import { connectDB } from "./lib/db.js";
 dotenv.config();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port} 🚀`);
   connectDB();
